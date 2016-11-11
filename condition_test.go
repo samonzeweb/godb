3a9fe2acd99b3217371307208dc2d58aab0202be
samonzeweb <sam.onzeweb@gmail.com>
2016-11-11 18:30:32 +0100

    Use prototype tests with goconvey

--- condition_test.go
+++ condition_test.go
@@ -1,118 +1,150 @@
 package godb
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
 
 func TestQ(t *testing.T) {
-	sql := "id = ?"
-	q := Q(sql, 123)
-	if q.sql != sql {
-		t.Error(q.sql, "!=", sql)
-	}
-	if len(q.args) != 1 {
-		t.Error("Wrong placeholder count :", len(q.args))
-	}
+	Convey("Q build", t, func() {
+		Convey("A simple condition with one placeholder and one argument", func() {
+			sql := "id = ?"
+			q := Q(sql, 123)
+			So(q.sql, ShouldEqual, sql)
+		})
 
-	sql = "id = ? AND is_deleted = ?"
-	q = Q(sql, 123, 0)
-	if q.sql != sql {
-		t.Error(q.sql, "!=", sql)
-	}
-	if len(q.args) != 2 {
-		t.Error("Wrong placeholder count :", len(q.args))
-	}
+		Convey("A simple condition with multiple placeholders arguments", func() {
+			sql := "id = ? AND is_deleted = ?"
+			q := Q(sql, 123, 0)
+			So(q.sql, ShouldEqual, sql)
+		})
 
-	sql = "id IN (?)"
-	q = Q(sql, []int{123, 456})
-	if q.sql != "id IN (?,?)" {
-		t.Error(q.sql, "!=", sql)
-	}
-	if len(q.args) != 2 {
-		t.Error("Wrong placeholder count :", len(q.args))
-	}
+		Convey("A condition expanding slice argument", func() {
+			q := Q("id IN (?)", []int{123, 456})
+			So(q.sql, ShouldEqual, "id IN (?,?)")
+		})
 
-	t.Log("Q check arguments count")
-	q = Q("id = ?", 123, 456)
-	if q.Error == nil {
-		t.Error("Q didn't produced an error when the argument count is incorrect")
-	}
+		Convey("A condition with slice and non slice arguments", func() {
+			q := Q("id IN (?) AND is_deleted = ?", []int{123, 456}, 0)
+			So(q.sql, ShouldEqual, "id IN (?,?) AND is_deleted = ?")
+		})
+	})
+
+	Convey("Q set error field if arguments and placeholders count does not math", t, func() {
+		Convey("A simple condition with one placeholder and one argument", func() {
+			sql := "id = ?"
+			q := Q(sql, 123, 456)
+			So(q.Error, ShouldNotBeNil)
+		})
+	})
 }
 
-func TestAnd(t *testing.T) {
-	c1 := Q("id = ?", 123)
-	c2 := Q("is_deleted = ?", 0)
-	q := And(c1, c2)
-	if q.sql != "id = ? AND is_deleted = ?" {
-		t.Error("And didn't build correct SQL :", q.sql)
-	}
-	if len(q.args) != 2 {
-		t.Error("And didn't build correct arguments, count :", len(q.args))
-	}
+func TestAND(t *testing.T) {
+	Convey("Given multiple conditions", t, func() {
+		c1 := Q("id = ?", 123)
+		c2 := Q("is_deleted = ?", 0)
 
-	q = And(c1)
-	if q.sql != c1.sql {
-		t.Error("And didn't build correct SQL :", q.sql)
-	}
-	if len(q.args) != 1 {
-		t.Error("And didn't build correct arguments, count :", len(q.args))
-	}
+		Convey("And conjunction joins conditions with 'AND'", func() {
+			So(And(c1, c2).sql, ShouldEqual, "id = ? AND is_deleted = ?")
+		})
 
-	q = And(Q("id = ?", 123, 456))
-	if q.Error == nil {
-		t.Error("And didn't propagated error")
-	}
+		Convey("And conjunction joins arguments", func() {
+			So(len(And(c1, c2).args), ShouldEqual, 2)
+		})
+	})
+
+	Convey("Given one condition", t, func() {
+		sql := "id = ?"
+		c := Q(sql, 123)
+
+		Convey("And conjunction return a similar condition", func() {
+			So(And(c).sql, ShouldEqual, sql)
+			So(len(And(c).args), ShouldEqual, 1)
+		})
+	})
+
+	Convey("Given conditions with at least one error", t, func() {
+		c := &Condition{Error: fmt.Errorf("A mysterious error has occurred")}
+
+		Convey("And return a condition with an error", func() {
+			So(And(c).Error, ShouldNotBeNil)
+		})
+	})
 }
 
-func TestOr(t *testing.T) {
-	c1 := Q("id = ?", 123)
-	c2 := Q("id = ?", 456)
-	q := Or(c1, c2)
-	if q.sql != "(id = ? OR id = ?)" {
-		t.Error("Or didn't build correct SQL :", q.sql)
-	}
-	if len(q.args) != 2 {
-		t.Error("Or didn't build correct arguments, count :", len(q.args))
-	}
+func TestOR(t *testing.T) {
+	Convey("Given multiple conditions", t, func() {
+		c1 := Q("id = ?", 123)
+		c2 := Q("id = ?", 456)
 
-	q = Or(c1)
-	if q.sql != c1.sql {
-		t.Error("Or didn't build correct SQL :", q.sql)
-	}
-	if len(q.args) != 1 {
-		t.Error("Or didn't build correct arguments, count :", len(q.args))
-	}
+		Convey("Or conjunction joins conditions with 'OR'", func() {
+			So(Or(c1, c2).sql, ShouldContainSubstring, "id = ? OR id = ?")
+		})
 
-	q = Or(Q("id = ?", 123, 456))
-	if q.Error == nil {
-		t.Error("Or didn't propagated error")
-	}
+		Convey("Or conjunction joins arguments", func() {
+			So(len(Or(c1, c2).args), ShouldEqual, 2)
+		})
+	})
+
+	Convey("Given one condition", t, func() {
+		sql := "id = ?"
+		c := Q(sql, 123)
+
+		Convey("Or conjunction return a similar condition", func() {
+			So(Or(c).sql, ShouldEqual, sql)
+			So(len(Or(c).args), ShouldEqual, 1)
+		})
+	})
+
+	Convey("Given conditions with at least one error", t, func() {
+		c := &Condition{Error: fmt.Errorf("A mysterious error has occurred")}
+
+		Convey("Or return a condition with an error", func() {
+			So(Or(c).Error, ShouldNotBeNil)
+		})
+	})
 }
 
-func TestNot(t *testing.T) {
-	q := Not(Q("is_deleted = ?", 0))
-	if q.sql != "NOT (is_deleted = ?)" {
-		t.Error("Not didn't build correct SQL :", q.sql)
-	}
-	if len(q.args) != 1 {
-		t.Error("Not didn't build correct arguments, count :", len(q.args))
-	}
+func TestNOT(t *testing.T) {
+	Convey("Given a condition", t, func() {
+		c := Q("id = ?", 123)
 
-	q = Not(Q("id = ?", 123, 456))
-	if q.Error == nil {
-		t.Error("Not didn't propagated error")
-	}
+		Convey("Not surround the condition with 'NOT (' and ')'", func() {
+			So(Not(c).sql, ShouldEqual, "NOT (id = ?)")
+		})
+
+		Convey("Not return a condition with the original arguments", func() {
+			So(len(Not(c).args), ShouldEqual, len(c.args))
+		})
+	})
+
+	Convey("Given a condition with an error", t, func() {
+		c := &Condition{Error: fmt.Errorf("A mysterious error has occurred")}
+
+		Convey("Not return a condition with an error", func() {
+			So(Not(c).Error, ShouldNotBeNil)
+		})
+	})
 }
 
 func TestAllTogether(t *testing.T) {
-	q := And(Q("id IN (?)", []int{1, 2, 3, 4, 5}),
-		Not(Q("is_deleted = ?", 1)),
-		Or(Q("category_id = ?", 123),
-			Q("tag IN (?)", []string{"foo", "bar", "baz"}),
-		),
-	)
-	if q.sql != "id IN (?,?,?,?,?) AND NOT (is_deleted = ?) AND (category_id = ? OR tag IN (?,?,?))" {
-		t.Error("Incorrect SQL :", q.sql)
-	}
-	if len(q.args) != 10 {
-		t.Error("Incorrect arguments, count :", len(q.args))
-	}
+	Convey("Given a 'complex' condition", t, func() {
+		q := And(Q("id IN (?)", []int{1, 2, 3, 4, 5}),
+			Not(Q("is_deleted = ?", 1)),
+			Or(Q("category_id = ?", 123),
+				Q("tag IN (?)", []string{"foo", "bar", "baz"}),
+			),
+		)
+
+		Convey("The SQL string is correct", func() {
+			sql := "id IN (?,?,?,?,?) AND NOT (is_deleted = ?) AND (category_id = ? OR tag IN (?,?,?))"
+			So(q.sql, ShouldEqual, sql)
+		})
+
+		Convey("The aruments count is correct", func() {
+			So(len(q.args), ShouldEqual, 10)
+		})
+	})
 }
--- select_statement_test.go
+++ select_statement_test.go
@@ -1,71 +1,306 @@
 package godb
 
-import "testing"
+import (
+	"testing"
 
-func TestSimpleSelectStatement(t *testing.T) {
-	selectStmt := newSelectStatement(&DB{}, "dummies").Columns("foo").Distinct()
+	. "github.com/smartystreets/goconvey/convey"
+)
 
-	sql, _, err := selectStmt.ToSQL()
-	checkToSQL(t, "SELECT DISTINCT foo FROM dummies", sql, err)
+func TestNewSelectStatement(t *testing.T) {
+	Convey("Create a select statement", t, func() {
+
+		Convey("Without columns", func() {
+			q := newSelectStatement(&DB{}, "dummies")
+			Convey("The table name is not empty", func() {
+				So(len(q.fromTables), ShouldEqual, 1)
+				So(q.fromTables[0], ShouldEqual, "dummies")
+			})
+		})
+	})
 }
 
-func TestSelectStatementWhere(t *testing.T) {
-	selectStmt := newSelectStatement(&DB{}, "dummies").
-		Columns("foo").
-		Where("id = ?", 123)
+func TestSelectColumns(t *testing.T) {
+	Convey("Given a select statement", t, func() {
+		q := newSelectStatement(&DB{}, "dummies")
 
-	sql, args, err := selectStmt.ToSQL()
-	checkToSQL(t, "SELECT foo FROM dummies WHERE id = ?", sql, err)
+		Convey("Columns add columns after the existing list", func() {
+			q.Columns("foo", "bar", "baz")
+			So(len(q.columns), ShouldEqual, 3)
+			So(q.columns[0], ShouldEqual, "foo")
+			So(q.columns[1], ShouldEqual, "bar")
+		})
 
-	if len(args) != 1 || args[0] != 123 {
-		t.Fatal("Bad arguments list :", args)
-	}
+	})
 }
 
-func TestSelectStatementWhereQ(t *testing.T) {
-	selectStmt := newSelectStatement(&DB{}, "dummies").
-		Columns("foo").
-		WhereQ(Q("id = ?", 1))
+func TestSelectFrom(t *testing.T) {
+	Convey("Given a select statement", t, func() {
+		q := newSelectStatement(&DB{}, "dummies").
+			Columns("foo", "bar", "baz")
 
-	sql, _, err := selectStmt.ToSQL()
-	checkToSQL(t, "SELECT foo FROM dummies WHERE id = ?", sql, err)
+		Convey("Calling From append a table name to the list", func() {
+			q.From("others")
+			So(len(q.fromTables), ShouldEqual, 2)
+			So(q.fromTables[0], ShouldEqual, "dummies")
+			So(q.fromTables[1], ShouldEqual, "others")
+		})
+	})
 }
 
-func TestSelectStatementGroupByHaving(t *testing.T) {
-	selectStmt := newSelectStatement(&DB{}, "dummies").
-		Columns("foo", "count(*)").
-		GroupBy("foo").
-		Having("count(*) > 1")
+func TestSelectLeftJoin(t *testing.T) {
+	Convey("Given a select query", t, func() {
+		q := newSelectStatement(&DB{}, "dummies").
+			Columns("foo", "bar", "baz")
 
-	sql, _, err := selectStmt.ToSQL()
-	checkToSQL(t, "SELECT foo, count(*) FROM dummies GROUP BY foo HAVING count(*) > 1", sql, err)
+		Convey("Calling LeftJoin will add the given string to the joins list", func() {
+			q.LeftJoin("others", "othersalias", Q("othersalias.id = dummies.other_id"))
+			So(len(q.joins), ShouldEqual, 1)
+		})
+	})
 }
 
-func TestSelectStatementOrderOffsetLimit(t *testing.T) {
-	selectStmt := newSelectStatement(&DB{}, "dummies").
-		Columns("foo", "bar").
-		OrderBy("foo").
-		Offset(2).
-		Limit(1)
+func TestSelectWhere(t *testing.T) {
+	Convey("Given a select query", t, func() {
+		q := newSelectStatement(&DB{}, "dummies").
+			Columns("foo", "bar", "baz")
 
-	sql, _, err := selectStmt.ToSQL()
-	checkToSQL(t, "SELECT foo, bar FROM dummies ORDER BY foo OFFSET ? LIMIT ?", sql, err)
+		Convey("Call Where will add a new condition", func() {
+			sql := "id = ?"
+			q.Where(sql, 123)
+			So(len(q.where), ShouldEqual, 1)
+			So(q.where[0].sql, ShouldEqual, sql)
+		})
+	})
 }
 
-func TestSelectStatementSuffix(t *testing.T) {
-	selectStmt := newSelectStatement(&DB{}, "dummies").
-		Columns("foo").
-		Suffix("FOR UPDATE")
+func TestSelectWhereQ(t *testing.T) {
+	Convey("Given a select query", t, func() {
+		q := newSelectStatement(&DB{}, "dummies").
+			Columns("foo", "bar", "baz")
 
-	sql, _, err := selectStmt.ToSQL()
-	checkToSQL(t, "SELECT foo FROM dummies FOR UPDATE", sql, err)
+		Convey("Call WhereQ will add the given condition", func() {
+			qc := Q("id = ?", 123)
+			q.WhereQ(qc)
+			So(len(q.where), ShouldEqual, 1)
+			So(q.where[0], ShouldEqual, qc)
+		})
+	})
 }
 
-func TestLeftJoin(t *testing.T) {
-	selectStmt := newSelectStatement(&DB{}, "dummies").
-		Columns("id", "foo", "other1.bar").
-		LeftJoin("other", "other1", Q("dummies.id = other1.dummy_id"))
+func TestSelectGroupBy(t *testing.T) {
+	Convey("Given a select query", t, func() {
+		q := newSelectStatement(&DB{}, "dummies").
+			Columns("foo", "count(*)")
 
-	sql, _, err := selectStmt.ToSQL()
-	checkToSQL(t, "SELECT id, foo, other1.bar FROM dummies LEFT JOIN other AS other1 ON dummies.id = other1.dummy_id", sql, err)
+		Convey("Calling GroupBy will add the given string to the groupBy list", func() {
+			groupBy := "foo"
+			q.GroupBy(groupBy)
+			So(len(q.groupBy), ShouldEqual, 1)
+			So(q.groupBy[0], ShouldEqual, groupBy)
+		})
+	})
+}
+
+func TestSelectHaving(t *testing.T) {
+	Convey("Given a select query", t, func() {
+		q := newSelectStatement(&DB{}, "dummies").
+			Columns("foo", "count(*)")
+
+		Convey("Call Having will add a new condition", func() {
+			sql := "count(*) > 1"
+			q.Having(sql)
+			So(len(q.having), ShouldEqual, 1)
+			So(q.having[0].sql, ShouldEqual, sql)
+		})
+	})
+}
+
+func TestSelectHavingQ(t *testing.T) {
+	Convey("Given a select query", t, func() {
+		q := newSelectStatement(&DB{}, "dummies").
+			Columns("foo", "count(*)")
+
+		Convey("Call WhereQ will add the given condition", func() {
+			qc := Q("count(*) > 1")
+			q.HavingQ(qc)
+			So(len(q.having), ShouldEqual, 1)
+			So(q.having[0], ShouldEqual, qc)
+		})
+	})
+}
+
+func TestSelectOrderBy(t *testing.T) {
+	Convey("Given a select query", t, func() {
+		q := newSelectStatement(&DB{}, "dummies").
+			Columns("foo", "bar", "baz")
+
+		Convey("Calling OrderBy will add the given string to the orderBy list", func() {
+			orderBy := "foo"
+			q.OrderBy(orderBy)
+			So(len(q.orderBy), ShouldEqual, 1)
+			So(q.orderBy[0], ShouldEqual, orderBy)
+		})
+	})
+}
+
+func TestSelectOffet(t *testing.T) {
+	Convey("Given a select query", t, func() {
+		q := newSelectStatement(&DB{}, "dummies").
+			Columns("foo", "bar", "baz")
+
+		Convey("Calling Offset will set the offset value", func() {
+			q.Offset(123)
+			So(q.offset, ShouldNotBeNil)
+			So(*q.offset, ShouldEqual, 123)
+		})
+	})
+}
+
+func TestSelectLimit(t *testing.T) {
+	Convey("Given a select query", t, func() {
+		q := newSelectStatement(&DB{}, "dummies").
+			Columns("foo", "bar", "baz")
+
+		Convey("Calling Limit will set the offset value", func() {
+			q.Limit(123)
+			So(q.limit, ShouldNotBeNil)
+			So(*q.limit, ShouldEqual, 123)
+		})
+	})
+}
+
+func TestSelectSuffix(t *testing.T) {
+	Convey("Given a select query", t, func() {
+		q := newSelectStatement(&DB{}, "dummies").
+			Columns("foo", "bar", "baz")
+
+		Convey("Calling Suffix will add the given string to the suffixes list", func() {
+			suffix := "FOR UPDATE"
+			q.Suffix(suffix)
+			So(len(q.suffixes), ShouldEqual, 1)
+			So(q.suffixes[0], ShouldEqual, suffix)
+		})
+	})
+}
+
+func TestSelectToSQL(t *testing.T) {
+	Convey("Given a select query with columns and table", t, func() {
+		q := newSelectStatement(&DB{}, "dummies").
+			Columns("foo", "bar", "baz")
+
+		Convey("ToSQL create a SQL request", func() {
+			sql, _, err := q.ToSQL()
+			So(err, ShouldBeNil)
+			So(sql, ShouldEqual, "SELECT foo, bar, baz FROM dummies")
+		})
+
+		Convey("Calling Distinct will add the distinct clause to SQL", func() {
+			q.Distinct()
+			sql, _, _ := q.ToSQL()
+			So(sql, ShouldStartWith, "SELECT DISTINCT")
+		})
+
+		Convey("Calling Join will add the specified join clause to SQL", func() {
+			q.LeftJoin("others", "othersalias", Q("othersalias.id = dummies.other_id"))
+			sql, _, _ := q.ToSQL()
+			So(sql, ShouldEndWith, "LEFT JOIN others AS othersalias ON othersalias.id = dummies.other_id")
+		})
+
+		Convey("Calling Where multiple times", func() {
+			q.Where("id = ?", 123).Where("is_deleted = ?", 0)
+			sql, args, _ := q.ToSQL()
+
+			Convey("will add all the specified conditions clause to SQL using the 'AND' conjunction", func() {
+				So(sql, ShouldEndWith, "WHERE id = ? AND is_deleted = ?")
+			})
+
+			Convey("will add given arguments in the correct order", func() {
+				So(len(args), ShouldEqual, 2)
+				So(args[0].(int), ShouldEqual, 123)
+				So(args[1].(int), ShouldEqual, 0)
+			})
+		})
+
+		Convey("Calling GroupBy will add the specified group by clause to SQL", func() {
+			q.GroupBy("foo").GroupBy("bar")
+			sql, _, _ := q.ToSQL()
+			So(sql, ShouldEndWith, "GROUP BY foo, bar")
+		})
+
+		Convey("Calling Having multiple times", func() {
+			q.GroupBy("foo")
+			q.Having("count(*) > ?", 0).Having("count(*) < ?", 10)
+			sql, args, _ := q.ToSQL()
+
+			Convey("will add all the specified conditions clause to SQL using the 'AND' conjunction", func() {
+				So(sql, ShouldEndWith, "count(*) > ? AND count(*) < ?")
+			})
+
+			Convey("will add given arguments in the correct order", func() {
+				So(len(args), ShouldEqual, 2)
+				So(args[0].(int), ShouldEqual, 0)
+				So(args[1].(int), ShouldEqual, 10)
+			})
+		})
+
+		Convey("Calling OrderBy will add the specified order by clause to SQL", func() {
+			q.OrderBy("foo").OrderBy("bar")
+			sql, _, _ := q.ToSQL()
+			So(sql, ShouldEndWith, "ORDER BY foo, bar")
+		})
+
+		Convey("Calling Offset will add the offset clause to SQL", func() {
+			q.Offset(10)
+			sql, args, _ := q.ToSQL()
+			So(sql, ShouldEndWith, "OFFSET ?")
+			So(args[0].(int), ShouldEqual, 10)
+		})
+
+		Convey("Calling Limit will add the limit clause to SQL", func() {
+			q.Limit(10)
+			sql, args, _ := q.ToSQL()
+			So(sql, ShouldEndWith, "LIMIT ?")
+			So(args[0].(int), ShouldEqual, 10)
+		})
+
+		Convey("Calling Suffix will add the given clause to SQL", func() {
+			q.Suffix("FOR UPDATE")
+			sql, _, _ := q.ToSQL()
+			So(sql, ShouldEndWith, "FOR UPDATE")
+		})
+
+	})
+}
+
+func TestSelectToSQLErrors(t *testing.T) {
+	Convey("Columns are mandatory", t, func() {
+		q := newSelectStatement(&DB{}, "dummies")
+		_, _, err := q.ToSQL()
+		So(err, ShouldNotBeNil)
+	})
+
+	Convey("Calling Having without GroupBy will returns an error", t, func() {
+		q := newSelectStatement(&DB{}, "dummies").
+			Columns("foo", "count(*)")
+		q.Having("count(*) > 1")
+		_, _, err := q.ToSQL()
+		So(err, ShouldNotBeNil)
+	})
+}
+
+func TestSelectPreparedStatement(t *testing.T) {
+	Convey("Given a valid select statement with nil as arguments", t, func() {
+		q := newSelectStatement(&DB{}, "dummies").
+			Columns("foo", "bar", "baz").
+			Where("id_deleted = ?", nil).
+			GroupBy("other_sutff").
+			Having("count(*) > ?", nil).
+			OrderBy("foo")
+			// Offset and Limit must have a real value for the moment
+
+		Convey("SQL is well builded", func() {
+			_, _, err := q.ToSQL()
+			So(err, ShouldBeNil)
+		})
+	})
 }
