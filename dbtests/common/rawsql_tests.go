fb17125a4507699d0fc8514b1ba4a0eb884e1539
samonzeweb <sam.onzeweb@gmail.com>
2018-01-21 17:46:16 +0100

    Added a complex raw query test using SQLBuilder

--- dbtests/common/rawsql_tests.go
+++ dbtests/common/rawsql_tests.go
@@ -24,9 +24,34 @@ func RawSQLTests(db *godb.DB, t *testing.T) {
 		}
 	}
 
-	// Test & assertions
+	// Tests & assertions
 	books := make([]Book, 0, 0)
 	err = db.RawSQL("select * from books where author = ?", authorAssimov).Do(&books)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(books) != len(setFoundation) {
+		t.Fatalf("Wrong books count : %d", len(books))
+	}
+
+	subQuery := godb.NewSQLBuffer(0, 0). // of course size can be zero
+						Write("select author ").
+						Write("from books ").
+						WriteCondition(godb.Q("where title = ?", bookFoundation.Title))
+
+	queryBuffer := godb.NewSQLBuffer(64, 0). // approximate size
+							Write("select * ").
+							Write("from books ").
+							Write("where author in (").
+							Append(subQuery).
+							Write(")")
+
+	if queryBuffer.Err() != nil {
+		t.Fatalf("Raw query building produce an error : %v", queryBuffer.Err())
+	}
+
+	err = db.RawSQL(queryBuffer.SQL(), queryBuffer.Arguments()...).Do(&books)
 	if err != nil {
 		t.Fatal(err)
 	}
