2d2a86be03ad65a1683e98f174ef4e7b3e6d8f6e
Erkan Durmus <erkan@eurotel.com.tr>
2018-05-01 16:23:04 +0300

    corrections

--- logger.go
+++ logger.go
@@ -5,9 +5,10 @@ import (
 	"fmt"
 )
 
-var logPrefix = "SQL:"
-var prefixedLog []interface{}
+const logPrefix = "SQL:"
 
+// Logger is interface for custom logger
+// Should implement `Println(v ...interface{})` method
 type Logger interface {
 	Println(v ...interface{})
 }
@@ -16,27 +17,26 @@ type Logger interface {
 // By default there is no logger.
 func (db *DB) SetLogger(logger Logger) {
 	db.logger = logger
-	prefixedLog = append(prefixedLog, logPrefix)
 }
 
 // logPrintln is a wrapper for log.Logger.Println with the DB.logger
 // as Logger.
 func (db *DB) logPrintln(v ...interface{}) {
 	if db.logger != nil {
-		db.logger.Println(append(prefixedLog,v)...)
+		db.logger.Println(logPrefix, v)
 	}
 }
 
 // logExecution adds a log with a duration and SQL statement.
 func (db *DB) logExecution(duration time.Duration, v ...interface{}) {
 	if db.logger != nil {
-		db.logger.Println(append(prefixedLog, v, fmt.Sprintf("(Duration: %v)", duration))...)
+		db.logger.Println(logPrefix, v, fmt.Sprintf("(Duration: %v)", duration))
 	}
 }
 
 // logExecution adds a log with a duration and SQL statement.
 func (db *DB) logExecutionErr(err error, v ...interface{}) {
 	if db.logger != nil {
-		db.logger.Println(append(prefixedLog, v, fmt.Sprintf("(ERROR: %v)", err))...)
+		db.logger.Println(logPrefix, v, fmt.Sprintf("(ERROR: %v)", err))
 	}
 }
